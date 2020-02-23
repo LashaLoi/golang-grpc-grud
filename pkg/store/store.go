e589a77e8b913e8ec996773678b578cdc90216c8
Aliaksei Loi <aliaksei.loi@itechart-group.com>
2020-02-23 11:11:28 +0300

    add goland config

---
+++ pkg/store/store.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+// Store ...
+type Store struct {
+	config *Config
+}
+
+// New ...
+func New(config *Config) *Store {
+	return &Store{config: config}
+}
+
+// Open ...
+func (s *Store) Open() error {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return nil
+}
+
+// Close ...
+func (s *Store) Close() {
+
+}
